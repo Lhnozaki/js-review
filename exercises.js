4cab0f76efccd8c5644bde717eaa1c05a8584462
Lewis Nozaki <lhnozaki@gmail.com>
2019-02-04 22:57:48 -1000

    Question 5

--- exercises.js
+++ exercises.js
@@ -53,6 +53,12 @@ Console log the variable
 Console log the value at index 1.
 Console log the value at index 3.*/
 
+let japanPrefectures = ['tokyo' , 'Osaka' , 'Kyoto' , 'Nara' , 'Hokkaido'];
+
+console.log("Here are a few Prefectures in Japan: " + japanPrefectures);
+console.log(japanPrefectures[1]);
+console.log(japanPrefectures[3]);
+
 /*6.  Variable with Array for its value.
 Declare a variable named planets and assign the value of each planet in our solar system, in the order of distance from the sun.  
 Console log the variable
