174763e98b7b0f5510e366d8bcd13b34ffb1f0f7
Lewis Nozaki <lhnozaki@gmail.com>
2019-02-07 18:31:11 -1000

    Question 12

--- exercises.js
+++ exercises.js
@@ -122,6 +122,15 @@ console.log(sameTwo === 3);
 
  console log the object*/
 
+let hotel = {
+   name: 'Hilton',
+   rooms: 200,
+   pool: true,
+   checkIn: null,
+}
+
+console.log(hotel);
+
 /*13.  Declare a literal object named "car" with properties and values set to:
 
     make  => "BMW"
