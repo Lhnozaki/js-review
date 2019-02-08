7490b7bd1e3dd090ceca92597e11c10060439761
Lewis Nozaki <lhnozaki@gmail.com>
2019-02-07 18:13:27 -1000

    Question 6

--- exercises.js
+++ exercises.js
@@ -65,6 +65,11 @@ Console log the variable
 Console log "Earth" using the index position
 Console log "Uranus" using the index position*/
 
+let planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+console.log('Here are the planets in our Solar System: ' + planets);
+console.log(planets[2]);
+console.log(planets[6]);
+
 /*7. Variables with Array for its value.
 Declare a variable named india and assign the value of 3 popular tourist attractions.  
 Console log the variable.*/
